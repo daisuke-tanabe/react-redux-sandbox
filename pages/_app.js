7f6b1676716511de0539e1b32ceaefb38a749c8d
田邉 大介 <helloworld0840@gmail.com>
2018-12-04 12:55:58 +0900

    chore: プロジェクトファイルの設置

---
+++ pages/_app.js
@@ -0,0 +1,21 @@
+// https://github.com/zeit/next.js/blob/canary/examples/with-redux/pages/_app.js
+
+import App, {Container} from 'next/app'
+import React from 'react'
+import withReduxStore from '../lib/with-redux-store'
+import { Provider } from 'react-redux'
+
+class MyApp extends App {
+  render () {
+    const {Component, pageProps, reduxStore} = this.props
+    return (
+      <Container>
+        <Provider store={reduxStore}>
+          <Component {...pageProps} />
+        </Provider>
+      </Container>
+    )
+  }
+}
+
+export default withReduxStore(MyApp)
