68a02855cd40b9f348e1e905fb5ed151c97e0533
田邉 大介 <helloworld0840@gmail.com>
2018-12-04 13:13:19 +0900

    chore: react学習用ファイルの設置

--- pages/index.js
+++ pages/index.js
@@ -1,1 +1,14 @@
-export default () => <div>Welcome to next.js!</div>
+import Link from 'next/link';
+
+export default () => (
+  <div>
+    <div>Welcome to next.js!</div>
+    <ul>
+      <li>
+        <Link href="/react">
+          <a>react</a>
+        </Link>
+      </li>
+    </ul>
+  </div>
+)
